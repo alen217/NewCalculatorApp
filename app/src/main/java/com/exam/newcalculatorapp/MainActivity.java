c79734917cd0fcf35b1b4685d39c03532c1f7859
alen217 <alen217fisat@gmail.com>
2024-12-18 13:40:02 +0530

    complete

--- app/src/main/java/com/exam/newcalculatorapp/MainActivity.java
+++ app/src/main/java/com/exam/newcalculatorapp/MainActivity.java
@@ -13,7 +13,7 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button ba;
+    Button ba,bs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,13 +21,24 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         ba=(Button) findViewById(R.id.ab);
+        bs=(Button) findViewById(R.id.sb);
 
+        bs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent o1=new Intent(getApplicationContext(), SubAct.class);
+                startActivity(o1);
+            }
+        });
         ba.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent o=new Intent(getApplicationContext(), Addact.class);
                 startActivity(o);
             }
-        });
+
+
+        }
+        );
     }
 }
